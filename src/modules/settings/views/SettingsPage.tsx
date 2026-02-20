29288163c137d7021ede60dd8ad584ed1bcf5014
MohammedAl-Nabahin <moha.nabahin@gmail.com>
2026-02-20 15:14:50 +0200

    Remove unused imports

--- src/modules/settings/views/SettingsPage.tsx
+++ src/modules/settings/views/SettingsPage.tsx
@@ -1,7 +1,1 @@
-// views/SettingsPage.tsx
-import { Outlet } from '@tanstack/react-router';
-import { Link } from '@tanstack/react-router';
-
-export function SettingsPage() {
-  
-}
+export function SettingsPage() {}
